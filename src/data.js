4dfab6246618def4e655f6d6ae25b97c552d6bf3
Demba Sow <dastech1998@gmail.com>
2021-11-06 23:22:13 +0000

    Finished the Product And Important links part

--- src/data.js
+++ src/data.js
@@ -1,32 +1,36 @@
+import ytb from "./images/ytb.png"
+import studyinturkey from "./images/studyinturkey.png"
+import atharan from "./images/atharan.png"
+
 export const products = [
-    {
-      id: 1,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 2,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 3,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 4,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 5,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 6,
-      img: "",
-      link: "http://lama.dev",
-    },
-  ];
+{
+    id: 1,
+    img: {ytb},
+    link: "https://www.turkiyeburslari.gov.tr/",
+},
+{
+    id: 2,
+    img:"https://im.ge/i/oEb1Aq",
+    link: "https://www-atharan-com/",
+},
+{
+    id: 3,
+    img:"https://im.ge/i/oEbd1P",
+    link: "https://studyinturkey.gov.tr/",
+},
+{
+    id: 4,
+    img:"https://upload.wikimedia.org/wikipedia/pt/0/0d/Thomas_Sankara.jpg",
+    link: "https://upload.wikimedia.org",
+},
+{
+    id: 5,
+    img: "",
+    link: "",
+},
+{
+    id: 6,
+    img: "",
+    link: "",
+},
+];
